f30cf72b5f264f4272c124d78b9158095e07bd2a
Accord33 <skb.ceg.biblia@gmail.com>
2022-08-08 14:34:51 +0900

    second commit

--- run.py
+++ run.py
@@ -9,7 +9,19 @@ def main(filepath, path):
         with open('news.json','r') as f:
             news = json.load(f)
         return render_template(f'{filepath}/{path}.html',news=news)
+    if path == 'blog':
+        with open('blog.json','r') as f:
+            blog = json.load(f)
+        return render_template(f'{filepath}/{path}.html', blog=blog)
     return render_template(f'{filepath}/{path}.html')
+
+@app.route('/.well-known/acme-challenge/B0ft-qLPlZyLTQRjFCiizzcY3HNQsXCZLpi7tcVKwCA')
+def ssl_encrypt():
+    return 'B0ft-qLPlZyLTQRjFCiizzcY3HNQsXCZLpi7tcVKwCA.58JW9ifPlEl4EL0CKnG3usMi_ppUDlDClVn9Ny8ke2o'
+
+@app.route('/blog/<page>')
+def blog(page):
+    return render_template(f'blog/{page}.html')
 
 @app.route('/news/<page>')
 def news(page):
